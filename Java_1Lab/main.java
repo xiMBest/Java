b086635fff6a6c5f778dbfcd602f5aead0d68650
xiMBest <33019802+xiMBest@users.noreply.github.com>
2018-06-10 12:22:19 +0300

    reformat code

--- Java_1Lab/main.java
+++ Java_1Lab/main.java
@@ -1,26 +1,23 @@
-package demo;
-
 public class main {
-    public static void main(String args[])
-    {
-        
-        
+    public static void main(String args[]) {
+
+
         Pool lvivPolitech = new Pool();
         Pool universityFranko = new Pool(30, 10000, 5, 10, "tile");
-        Pool catholicUniversity = new Pool (50, 20000, 10, 10, "tile");
-        
+        Pool catholicUniversity = new Pool(50, 20000, 10, 10, "tile");
+
         System.out.println(lvivPolitech.toString());
         System.out.println(universityFranko.toString());
         System.out.println(catholicUniversity.toString());
-        
+
         lvivPolitech.resetValues(70, 15000, 5, 10, "tile");
-        
+
         lvivPolitech.printSum();
-        univeersityFranko.printSum();
+        universityFranko.printSum();
         catholicUniversity.printSum();
-        
+
         universityFranko.setCapacity(23000);
-        
+
         Pool.printStaticSum();
     }
 }
